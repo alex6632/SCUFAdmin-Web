c614ea1c079f600e6112e2c8ea0d1bbe18a9fde9
Alexandre Simonin <a.simonin69@gmail.com>
2018-05-02 11:46:23 +0200

    ADD profile page

--- www/js/app.js
+++ www/js/app.js
@@ -129,6 +129,7 @@ var me = {
     } else {
       $('.loginTrigger .login').remove();
       $('.loginTrigger').addClass('hide');
+      me.getSetting('coeff', localStorage.getItem('authTokenVALUE'));
     }
   },
 
@@ -218,6 +219,26 @@ var me = {
   },
 
   /*
+   * GET ONE SETTING
+   */
+  getSetting: function(element, authTokenVALUE) {
+    var api = "http://127.0.0.1:8000/setting/" + element;
+    $.ajax({
+      url: api,
+      type: 'GET',
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader('X-Auth-Token', authTokenVALUE);
+      },
+      success: function (response) {
+        localStorage.setItem('settingCOEFF', response.value);
+      },
+      error: function (response) {
+        console.log(response);
+      }
+    });
+  },
+
+  /*
    * PROFIL PAGE
    */
   profile: function (authTokenVALUE, userID) {
@@ -231,6 +252,36 @@ var me = {
       success: function (response) {
         console.log("Profile response : ",response);
         $('.profile-page__name').text(response.firstname + " " + response.lastname);
+        var role = "";
+        switch (response.role) {
+          case 1:
+            role = 'Salarié';
+            break;
+          case 2:
+            role = 'Manager';
+            break;
+          case 3:
+            role = 'Superviseur';
+            break;
+          case 4:
+            role = 'Administrateur';
+            break;
+          default:
+            role = "Aucun";
+        }
+        var hoursToPlanify = response.hoursTodo - response.hoursPlanified;
+        var percentageHoursTodo = (response.hoursDone / response.hoursTodo) * 100;
+        var percentageHoursPlanified = (response.hoursPlanifiedByMe / hoursToPlanify) * 100;
+        var coeff = localStorage.getItem('settingCOEFF');
+        var repos = response.overtime / coeff;
+        $('.profile-page__status').text(role);
+        $('#jsHoursTodo .progress-bar__ratio').html('<span class="ratio-ok">' + response.hoursDone + '</span>/' + response.hoursTodo + '<span>H</span>');
+        $('#jsHoursTodo .progress-bar__bar__text, #jsHoursTodo .progress-bar__bar__wip span').text(percentageHoursTodo + '%');
+        $('#jsHoursTodo .progress-bar__bar__wip').attr('style', 'width: ' + percentageHoursTodo + '%;');
+        $('#jsHoursPlanified .progress-bar__ratio').html('<span class="ratio-ok">' + response.hoursPlanifiedByMe + '</span>/' + hoursToPlanify + '<span>H</span>');
+        $('#jsHoursPlanified .progress-bar__bar__text, #jsHoursPlanified .progress-bar__bar__wip span').text(percentageHoursPlanified + '%');
+        $('#jsHoursPlanified .progress-bar__bar__wip').attr('style', 'width: ' + percentageHoursPlanified + '%;');
+        $('.profile-page__info-hours__nb').html(repos + '<span>H</span>');
       },
       error: function (response) {
         console.log(response);
